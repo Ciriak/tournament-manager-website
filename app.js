52847ea2a6545b31720c87654b1c3237826c59c9
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-06-21 14:39:20 +0200

    initialize project structure

---
+++ app.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var request = require('request');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+
+//edit the api location here
+var apiIndex = "http://colab.laouiti.me/app_dev.php";
+
+app.use(express.static('public'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+// parse application/json
+app.use(bodyParser.json({limit: '50mb'}));
+
+// Add headers
+app.use(cors());
+
+app.all('/api/*', function (req, res) {
+  var reqPath = req.url.replace("/api","");
+  console.log("-- API Call --");
+  console.log(req.method+" on "+apiIndex+reqPath);
+
+  //replace the calling uri
+  //req.headers.host = apiIndex+reqPath;
+
+
+  var options = {
+    url : apiIndex+reqPath,
+    method: req.method,
+    formData: req.body
+  }
+
+
+    if(req.query.access_token){
+      console.log("Authentification with token : ");
+      console.log(req.query.access_token);
+      options.headers = {};
+      options.headers['Authorization'] = "Bearer "+req.query.access_token;
+      delete req.query.access_token;
+    }
+
+  request(options,function(req,response,body){
+    console.log("-> Status "+response.statusCode);
+    console.log("--------------");
+    console.log("Form-data");
+    console.log(options.formData);
+    res.status(response.statusCode).send(body);
+  })
+});
+
+app.listen(8080, function () {
+  console.log('Projet2 is active on port 8080');
+});
---
+++ src/js/app.js
@@ -0,0 +1,189 @@
+var app = angular.module('projet2', [
+'ui.router',
+'ui.bootstrap',
+'LocalStorageModule'
+    ]);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /
+  $urlRouterProvider.otherwise("/");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('main', {
+      url: "/",
+      templateUrl: "views/index.html",
+      reload:true
+    })
+    .state('signup', {
+      url: "/signup",
+      templateUrl: "views/signup.html",
+      controller : "signupCtrl"
+    })
+    .state('signup.endingSignup', {
+      url: "/ending/:token",
+      templateUrl: "views/ending_signup.html"
+    })
+    .state('resetPassword', {
+      url: "/reset_password",
+      templateUrl: "views/reset_password.html",
+      controller : "resetPasswordCtrl"
+    })
+    .state('login', {
+      url: "/login",
+      templateUrl: "views/login.html",
+      controller : "loginCtrl"
+    })
+    .state('organizations', {
+      url: "/organizations",
+      templateUrl: "views/organizations/list.html",
+      controller : "organizationCtrl"
+    })
+    .state('users', {
+      url: "/users",
+      templateUrl: "views/users.html",
+      controller : "accountCtrl"
+    })
+    .state('editProfil', {
+      url: "/profile/edit",
+      templateUrl: "views/edit_profil.html",
+      controller : "accountCtrl"
+    })
+    .state('contact', {
+      url: "/contact",
+      templateUrl: "views/contact.html",
+      controller : "contactCtrl"
+    })
+    .state('projects', {
+      url: "/projects",
+      templateUrl: "views/projects/list.html",
+      controller : "projectCtrl"
+    })
+    .state('projects.item', {
+      url: "/{id:int}",
+      templateUrl: "views/projects/project.html",
+    })
+    .state('projects.item.promise', {
+      url: "/{promiseId:int}",
+      templateUrl: "views/projects/promise.html",
+    })
+    .state('newProject', {
+      url: "/new_project",
+      templateUrl: "views/projects/new.html",
+      controller : "projectCtrl"
+    })
+    /*.state('project', {
+      url: "/project",
+      templateUrl: "views/projects/project.html",
+    })*/
+    .state('announcements', {
+      url: "/announcements",
+      templateUrl: "views/announcements/list.html",
+      controller : "announcementCtrl"
+    })
+    .state('announcements.item', {
+      url: "/{id:int}",
+      templateUrl: "views/announcements/announcement.html",
+    })
+    .state('newAnnouncement', {
+      url: "/new_announcement",
+      templateUrl: "views/announcements/new.html",
+      controller : "announcementCtrl"
+    })
+    .state('search', {
+      url: "/search/:query",
+      templateUrl: "views/search.html",
+      controller : "searchCtrl"
+    });
+});
+
+app.directive("fileread", [function () {
+    return {
+        scope: {
+            fileread: "="
+        },
+        link: function (scope, element, attributes) {
+            element.bind("change", function (changeEvent) {
+                var reader = new FileReader();
+                reader.onload = function (loadEvent) {
+                    scope.$apply(function () {
+                        scope.fileread = loadEvent.target.result;
+                    });
+                }
+                reader.readAsDataURL(changeEvent.target.files[0]);
+            });
+        }
+    }
+}]);
+
+app.config(function (localStorageServiceProvider) {
+  localStorageServiceProvider
+    .setPrefix('colab');
+});
+
+app.controller('mainCtrl', ['$scope', '$http','$rootScope','$location','$state','localStorageService', function($scope, $http,$rootScope,$location,$state,localStorageService)
+{
+  var apiAddress = "http://"+$location.host()+":"+$location.port()+"/api";
+
+  $rootScope.apiAddress = apiAddress;
+
+  $rootScope.access_token = "";
+  $rootScope.logged = false;
+
+  $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
+    // called when a state change
+  });
+  $rootScope.$on('$stateChangeSuccess', function (event) {
+    // called when a state has changed
+  });
+
+  //
+  //  Login checker
+  //
+
+  var at = localStorageService.get("access_token");
+  if(at){
+    console.log("Login...");
+    $rootScope.access_token = at;
+    $rootScope.logged = true;
+  }
+
+  $rootScope.logout = function(){
+    if(confirm("Voulez-vous vraiment vous déconnecter ?")){
+      $rootScope.access_token = "";
+      $rootScope.logged = false;
+      localStorageService.remove("access_token");
+      location.reload();
+    }
+  }
+
+
+if($rootScope.logged){
+  var apiUri = $rootScope.apiAddress+'/me'+'?access_token='+$rootScope.access_token;
+  $http({
+    method: 'GET',
+    url: apiUri
+  }).then(function successCallback(response) {
+    console.log(response);
+    $rootScope.me = response.data;
+    }, function errorCallback(response) {
+      console.log(response);
+      if($rootScope.access_token){
+        $state.go('signup.endingSignup');
+      }
+    });
+}
+
+
+  $(".menu-btn").click(function(){
+    $(this).toggleClass("opened");
+    $(".menu").toggleClass("opened");
+  });
+
+   $(".menu li a").click(function(){
+    $(".menu-btn").removeClass("opened");
+    $(".menu").removeClass("opened");
+  });
+
+}]);
