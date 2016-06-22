6e7261e0119056e266f5f05f1cea8acb6e61062f
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-06-22 12:16:23 +0200

    Added login page and controller, fix some block positioning

---
+++ src/js/controllers/login.js
@@ -0,0 +1,38 @@
+app.controller('loginCtrl', ['$scope', '$http','$rootScope','$location','$state','localStorageService', function($scope, $http,$rootScope,$location,$state,localStorageService)
+{
+  $scope.login = {
+    processing : false,
+    buttonLabel : "Connexion",
+    error : false
+  };
+
+  $scope.signup = {
+    processing : false,
+    buttonLabel : "Créer mon compte",
+    error : false
+  }
+
+  $scope.signup.send = function(){
+    var oldLabel = this.buttonLabel;
+    this.processing = true;
+    this.buttonLabel = "Patientez...";
+  };
+
+  $scope.login.send = function(){
+    var oldLabel = this.buttonLabel;
+    this.processing = true;
+    this.buttonLabel = "Patientez...";
+    $http({
+      method: 'POST',
+      url: $rootScope.apiAddress+'/login_check',
+      data : $scope.login
+    }).then(function successCallback(r) {
+      $state.go('main');
+      location.reload();
+    }, function errorCallback(r) {
+      $scope.login.error = "Echec de la connexion";
+      $scope.login.processing = false;
+      $scope.login.buttonLabel = oldLabel;
+    });
+  };
+}]);
