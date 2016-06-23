4e3a925533365de121e0c4d7a3ed34126424717e
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-06-23 14:50:51 +0200

    Added profil and edit profil controller

---
+++ src/js/controllers/profil.js
@@ -0,0 +1,15 @@
+app.controller('profilCtrl', ['$scope', '$http','$rootScope','$location','$state','localStorageService', function($scope, $http,$rootScope,$location,$state,localStorageService)
+{
+
+  if(!$rootScope.me){
+    $state.go('login');
+    return;
+  }
+  $scope.profil = {
+    processing : false,
+    buttonLabel : "Sauvegarder les modifications",
+    error : false,
+    success : false
+  };
+
+}]);
