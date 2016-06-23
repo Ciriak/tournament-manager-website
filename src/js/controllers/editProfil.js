6e2c5a21edd9cd9f1f138a9a5233422eb331bb3f
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-06-23 15:17:06 +0200

    Updated edit profil controller

--- src/js/controllers/editProfil.js
+++ src/js/controllers/editProfil.js
@@ -5,11 +5,45 @@ app.controller('editProfilCtrl', ['$scope', '$http','$rootScope','$location','$s
     $state.go('login');
     return;
   }
+
+
   $scope.profil = {
     processing : false,
     buttonLabel : "Sauvegarder les modifications",
     error : false,
     success : false
+  };
+
+  //add every $me param to $profil
+  for (v in $scope.me) {
+    $scope.profil[v] = $scope.me[v];
+  }
+
+  $scope.profil.save = function(){
+    var oldLabel = this.buttonLabel;
+    this.success = false;
+    this.error = false;
+    this.processing = true;
+    this.buttonLabel = "Patientez...";
+    $http({
+      method: 'PATCH',
+      url: $rootScope.apiAddress+'/me?access_token='+$rootScope.access_token,
+      data : $scope.profil
+    }).then(function successCallback(r) {
+      $scope.profil.processing = false;
+      $scope.profil.buttonLabel = oldLabel;
+      $scope.profil.success = true;
+    }, function errorCallback(r) {
+      if(r.data.error){
+        $scope.profil.error = r.data.error.exception[0].message;
+      }
+      else{
+        $scope.profil.error = "Une erreur s'est produite lors de la sauvegarde !";
+      }
+
+      $scope.profil.processing = false;
+      $scope.profil.buttonLabel = oldLabel;
+    });
   };
 
 }]);
