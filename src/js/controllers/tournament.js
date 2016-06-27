8e3da0066bfd38211a32f69de92923c8d74da53c
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-06-27 16:14:58 +0200

    Added the "generating tournament" function and view

--- src/js/controllers/tournament.js
+++ src/js/controllers/tournament.js
@@ -4,7 +4,9 @@ app.controller('tournamentCtrl', ['$scope', '$http','$rootScope','$location','$s
     processing : false,
     buttonLabel : "S'inscrire",
     error : false,
-    canSuscribe : false
+    canSuscribe : false,
+    isAdmin : false,
+    canGenerate : false
   };
 
   $http({
@@ -15,7 +17,16 @@ app.controller('tournamentCtrl', ['$scope', '$http','$rootScope','$location','$s
     $scope.tournament.players_count = $scope.tournament.accounts.length;
 
     var i = _.findLastIndex($scope.tournament.accounts, { 'id': $scope.me.id });
-    console.log(i);
+
+    //check if current user is the admin
+    if($scope.tournament.account.id === $scope.me.id){
+      $scope.tournamentOpt.isAdmin = true;
+      //the tournament is full and can be generated
+      if($scope.tournament.players_count === $scope.tournament.max_players){
+        $scope.tournamentOpt.canGenerate = true;
+      }
+    }
+
     if(i >= 0){
       $scope.tournamentOpt.canSuscribe = false;
       $scope.tournament.accounts[i].me = true;
@@ -31,6 +42,8 @@ app.controller('tournamentCtrl', ['$scope', '$http','$rootScope','$location','$s
       $state.go('main');
   });
 
+
+  //join the tournament
   $scope.tournamentOpt.join = function(){
     if(!$scope.tournamentOpt.canSuscribe){
       return;
@@ -49,6 +62,22 @@ app.controller('tournamentCtrl', ['$scope', '$http','$rootScope','$location','$s
         $scope.tournamentOpt.processing = false;
         $scope.tournamentOpt.buttonLabel = obl;
         console.log("Unable to register to this tournament");
+    });
+  }
+
+  //join the tournament
+  $scope.tournamentOpt.generate = function(){
+    if(!$scope.tournamentOpt.canGenerate){
+      return;
+    }
+
+    $http({
+      method: 'POST',
+      url: $rootScope.apiAddress+'/tournaments/'+$state.params.id+'/validates?access_token=' + $rootScope.access_token
+    }).then(function successCallback(r) {
+      location.reload();
+    }, function errorCallback(r) {
+        console.log("Unable to generate this tournament");
     });
   }
 
