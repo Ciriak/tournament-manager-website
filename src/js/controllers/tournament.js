000e8bc904e80b4829aa35367b41fb6bbe5de3fd
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-06-27 12:31:10 +0200

    Can now join a tournament / fix the "new tournament" link

--- src/js/controllers/tournament.js
+++ src/js/controllers/tournament.js
@@ -1,11 +1,10 @@
 app.controller('tournamentCtrl', ['$scope', '$http','$rootScope','$location','$state','localStorageService', function($scope, $http,$rootScope,$location,$state,localStorageService)
 {
-  $scope.isMine = false;
-  if($rootScope.me){
-    if($rootScope.me.id === $state.params.id){
-      $scope.isMine = true;
-    }
-  }
+  $scope.tournamentOpt = {
+    processing : false,
+    buttonLabel : "S'inscrire",
+    error : false
+  };
 
   $http({
     method: 'GET',
@@ -17,5 +16,22 @@ app.controller('tournamentCtrl', ['$scope', '$http','$rootScope','$location','$s
       console.log("This tournament does no exist !");
       $state.go('main');
   });
+
+  $scope.tournamentOpt.join = function(){
+    var obl = $scope.tournamentOpt.buttonLabel;
+    $scope.tournamentOpt.buttonLabel = "Inscription...";
+    $scope.tournamentOpt.processing = true;
+    $http({
+      method: 'POST',
+      url: $rootScope.apiAddress+'/tournaments/'+$state.params.id+'/registers?access_token=' + $rootScope.access_token
+    }).then(function successCallback(r) {
+      location.reload();
+      console.log(r.data);
+    }, function errorCallback(r) {
+        $scope.tournamentOpt.processing = false;
+        $scope.tournamentOpt.buttonLabel = obl;
+        console.log("Unable to register to this tournament");
+    });
+  }
 
 }]);
