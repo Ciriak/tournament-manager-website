4cd59b06480af58e09b389f4eee345bf2de5ea0f
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-06-29 15:42:53 +0200

    Added comment section and controller in profil

--- src/js/controllers/profil.js
+++ src/js/controllers/profil.js
@@ -24,4 +24,42 @@ app.controller('profilCtrl', ['$scope', '$http','$rootScope','$location','$state
     });
   }
 
+  $scope.comment = {
+    processing : false,
+    error : false,
+    text : null
+  }
+
+  $scope.comment.post = function(){
+    this.processing = true;
+    this.error = false;
+    $http({
+      method: 'POST',
+      url: $rootScope.apiAddress+'/comments/'+$state.params.id+'?access_token=' + $rootScope.access_token
+    }).then(function successCallback(r) {
+      $scope.comment.text = "";
+      $scope.comment.processing = false;
+    }, function errorCallback(r) {
+      $scope.comment.processing = false;
+      $scope.comment.error = "Une erreur s'est produite lors du post du commentaire";
+    });
+  };
+
+  $scope.comment.delete = function(comment){
+    if(!$scope.isMine){
+      return;
+    }
+
+    $http({
+      method: 'DELETE',
+      url: $rootScope.apiAddress+'/comments/'+comment+'?access_token=' + $rootScope.access_token
+    }).then(function successCallback(r) {
+      $("#comment-"+comment).fadeOut();
+    }, function errorCallback(r) {
+      $scope.comment.processing = false;
+      $scope.comment.error = "Une erreur s'est produite lors du post du commentaire";
+    });
+
+  };
+
 }]);
