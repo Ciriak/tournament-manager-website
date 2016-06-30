b9c0a5a8881fd64d082a3120357c000f8f9ad116
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-06-30 21:17:57 +0200

    Added index function / stats and added logo / favicon

--- src/js/controllers/profilList.js
+++ src/js/controllers/profilList.js
@@ -9,7 +9,8 @@ app.controller('profilListCtrl', ['$scope', '$http','$rootScope','$location','$s
     url: $rootScope.apiAddress+'/accounts?access_token=' + $rootScope.access_token
   }).then(function successCallback(r) {
     $scope.profils = r.data;
-    $scope.topUsers = _.orderBy($scope.profils.slice(0, 5), ['user', 'age'], ['asc', 'desc']);
+    var t = _.orderBy($scope.profils, ['level.level'], ['desc']);
+    $scope.topUsers = t.slice(0,5);
     console.log(r.data);
   }, function errorCallback(r) {
       console.log(r);
